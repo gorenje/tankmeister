470b3379bc27afb81dc00c1df5b75ea787f3c72f
Gerrit Riessen <gerrit.riessen@gmail.com>
2016-12-27 17:57:08 +0100

    auto update becomes auto notification - webapps and UI threading

--- public/j/automatic_update.js
+++ public/j/automatic_update.js
@@ -1,16 +1,44 @@
-function autoUpdateMap() {
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getCarAndCityData);
+  }
+}
+
+function getCarAndCityData(position){
+  var lat = position.coords.latitude,
+      lng = position.coords.longitude;
+
+  $.ajax({
+    url: "/city?lat=" + lat + "&lng=" + lng + "&csc=" + csc,
+    method: 'get',
+    dataType: 'json'
+  }).done(function(city){
+    $.ajax({
+      url: "/nearest?lat=" + lat + "&lng=" + lng + "&cid=" + city.cityid + 
+               "&csc=" + csc,
+      method: 'get',
+      dataType: 'json'
+    }).done(function(data){
+       $.each(data.cars, function(idx, car) {
+         notifyCloseCars(car);
+       });
+    });
+  });
+}
+
+function autoNotification() {
   try {
-    updateLocation();
+    getLocation();
   } catch (x) {
     console.log(x);
   }
-  setTimeout( autoUpdateMap, 10000 );
+  setTimeout(autoNotification, 10000);
 }
 
 $(document).ready(function(){
   $('#autoupdate').change(function() {
      if(this.checked) {
-       setTimeout( autoUpdateMap, 10000 );
+       setTimeout( autoNotification, 10000 );
      }
  });
 });
