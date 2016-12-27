b079eb8b447a74f6dbfaf564b93d947ba4958385
Gerrit Riessen <gerrit.riessen@gmail.com>
2016-12-27 18:55:21 +0100

    better handling of notifications

--- public/j/notify.js
+++ public/j/notify.js
@@ -1,17 +1,19 @@
-function notifyMessage(msg) {
+function notifyMessage(msg,opts) {
   if (!("Notification" in window)) {
     return;
   }
 
   else if (Notification.permission === "granted") {
     // If it's okay let's create a notification
-    var notification = new Notification(msg);
+    var notification = new Notification(msg,opts);
+    setTimeout(notification.close.bind(notification), 60000); 
   }
 
   else if (Notification.permission !== 'denied') {
     Notification.requestPermission(function (permission) {
       if (permission === "granted") {
-        var notification = new Notification(msg);
+        var notification = new Notification(msg,opts);
+        setTimeout(notification.close.bind(notification), 60000); 
       }
     });
   }
@@ -19,6 +21,10 @@ function notifyMessage(msg) {
 
 function notifyCloseCars(car) {
   if ( car.distance < 800 ) { // distance is measured in meters
-    notifyMessage("Car " + car.name + " is in the vicinity");
+    var options = {
+      body: "Car " + car.name + " is in the vicinity",
+      icon: '/images/appicon_96x96.png'
+    };
+    notifyMessage("Carsharing", options);
   }
 }
