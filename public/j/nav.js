47afbe8b0f36eb819c629094492a0a7e1a9c64da
Gerrit Riessen <gerrit.riessen@gmail.com>
2017-02-08 11:04:25 +0100

    fixed radius selector and made provider refresh work
    On branch: changes

--- public/j/nav.js
+++ public/j/nav.js
@@ -7,13 +7,3 @@ function showPleaseWait() {
   $('#retrybutton').slideUp().
     fadeOut({complete: function(){$('#pleasewait').fadeIn();}});
 }
-
-function changeCsc(event) {
-  event.preventDefault();
-  $('#timestamp').hide().html("");
-  if ( $('#autoupdate').prop('checked') ) { $('#autoupdate').click(); }
-  if ( $('#autonotify').prop('checked') ) { $('#autonotify').click(); }
-  $('#mainmap').slideUp().
-    fadeOut({ complete: function(){ $('#mainhowto').slideDown().fadeIn();}});
-}
-
