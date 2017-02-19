edb0c6ef2ac276f860f9b2714624e10bb912f5cc
Gerrit Riessen <gerrit.riessen@gmail.com>
2017-02-19 19:01:47 +0100

    use the push.js library for notifications - https://nickersoft.github.io/push.js/
    On branch: master

--- public/j/notify.js
+++ public/j/notify.js
@@ -1,37 +1,15 @@
-function notifyMessage(msg,opts,car) {
-  if (!("Notification" in window)) {
-    return;
-  }
-
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    var notification = new Notification(msg,opts);
-    notification.onclick = function(){
-      window.open(car.reserve_link);
-    };
-    setTimeout(notification.close.bind(notification), 60000); 
-  }
-
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      if (permission === "granted") {
-        var notification = new Notification(msg,opts);
-        setTimeout(notification.close.bind(notification), 60000); 
-      }
-    });
-  }
-}
-
 function notifyCloseCars(car) {
   // distance is measured in meters
   if ( car.distance < parseInt($('#radiusselector').val()) ) {
-    var options = {
-      body: "Car " + car.name + " is in the vicinity (" +
-        car.distance.toFixed(0) + "m)",
-      icon: 'images/appicon_48x48.png',
-      tag: car.name,
-      renotify: false
-    };
-    notifyMessage("Tankmeister", options, car);
+    Push.create(car.name, {
+       body: "Car " + car.name + " is in the vicinity (" +
+                       car.distance.toFixed(0) + "m)",
+       icon: 'images/appicon_48x48.png',
+       timeout: 4000,
+       onClick: function () {
+           window.open(car.reserve_link);
+           this.close();
+       }
+   });
   }
 }
